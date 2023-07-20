c398eac1627c05cd33056739f0076f527a7e96a9
fabricio.polonio@gmail.com <fabriciopolonio@Ubuntu.myguest.virtualbox.org>
2023-07-21 01:22:05 +0200

    Small update on define order

--- configs/NBD_INFINITYAIO/config.h
+++ configs/NBD_INFINITYAIO/config.h
@@ -30,6 +30,8 @@
 #define USE_ACC_SPI_MPU6000
 #define USE_GYRO
 #define USE_GYRO_SPI_MPU6000
+#define USE_OSD
+#define USE_SPI_GYRO
 
 #define BEEPER_PIN           PC15
 #define MOTOR1_PIN           PB4
@@ -74,8 +76,6 @@
 #define DEFAULT_CURRENT_METER_SCALE 230
 #define BEEPER_INVERTED
 #define FLASH_SPI_INSTANCE SPI1
-#define USE_OSD
-#define USE_SPI_GYRO
 #define GYRO_1_SPI_INSTANCE SPI2
 #define GYRO_1_ALIGN CW180_DEG
 #define GYRO_1_ALIGN_YAW 1800
