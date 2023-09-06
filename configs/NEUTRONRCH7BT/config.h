e30e87d3cb52bf3b30a23f5686e420cde08d79ad
linjieqiang <517503838@qq.com>
2023-09-06 23:06:49 +0800

    Maintain target board: Use the USE_FLASH_W25N01G

--- configs/NEUTRONRCH7BT/config.h
+++ configs/NEUTRONRCH7BT/config.h
@@ -28,7 +28,7 @@
 #define USE_ACC
 #define USE_ACCGYRO_BMI270
 #define USE_FLASH
-#define USE_FLASH_W25M02G
+#define USE_FLASH_W25N01G
 #define USE_MAX7456
 #define USE_BARO_DPS310
 #define USE_MAG_QMC5883
