97222403904d788802865afedfea282397866eee
Guidus93 <guido.fabbrini@gmail.com>
2023-11-11 09:39:22 +0800

    added BARO (BMP280 and DPS310) define

--- configs/MEPSF411/config.h
+++ configs/MEPSF411/config.h
@@ -30,6 +30,8 @@
 #define USE_GYRO_SPI_MPU6500
 #define USE_ACC
 #define USE_ACC_SPI_MPU6500
+#define USE_BARO_BMP280
+#define USE_BARO_DPS310
 #define USE_FLASH
 #define USE_FLASH_M25P16
 #define USE_MAX7456
