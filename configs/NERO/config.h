68b2ae5b3dc41d03971f5e47e2dc80fb990cd0f5
Mark Haslinghuis <mark@numloq.nl>
2024-04-02 18:08:05 +0200

    Update NERO

--- configs/NERO/config.h
+++ configs/NERO/config.h
@@ -30,42 +30,70 @@
 #define USE_ACC_SPI_MPU6500
 #define USE_GYRO
 #define USE_GYRO_SPI_MPU6500
+#define USE_SDCARD
 
+// Basic I/O
 #define LED0_PIN             PB6
 #define LED1_PIN             PB5
 #define LED2_PIN             PB4
 #define BEEPER_PIN           PC1
-#define I2C1_SCL_PIN         PB8
-#define I2C1_SDA_PIN         PB9
-#define SPI1_SCK_PIN         PA5
-#define SPI1_SDI_PIN         PA6
-#define SPI1_SDO_PIN         PA7
-#define SPI2_SCK_PIN         PB13
-#define SPI2_SDI_PIN         PB14
-#define SPI2_SDO_PIN         PB15
-#define SPI3_SCK_PIN         PC10
-#define SPI3_SDI_PIN         PC11
-#define SPI3_SDO_PIN         PC12
-#define GYRO_1_CS_PIN        PC4
-#define GYRO_1_EXTI_PIN      PB15
-#define SDCARD_SPI_CS_PIN    PA15
-#define SDCARD_DETECT_PIN    PD2
+#define BEEPER_INVERTED
+#define LED_STRIP_PIN        PB0 // Shared with MOTOR5_PIN
+#define ESCSERIAL_PIN        PC7
+
+// Motors
 #define MOTOR1_PIN           PA0
 #define MOTOR2_PIN           PA1
 #define MOTOR3_PIN           PA2
 #define MOTOR4_PIN           PA3
-#define LED_STRIP_PIN        PB0
+#define MOTOR5_PIN           PB0 // Shared with LED_STRIP_PIN
+#define MOTOR6_PIN           PB1
+#define MOTOR7_PIN           PC8
+#define MOTOR8_PIN           PC9
 #define RX_PPM_PIN           PC7
+#define DEFAULT_DSHOT_BURST DSHOT_DMAR_ON
+
+// UART
 #define UART1_TX_PIN         PA9
 #define UART1_RX_PIN         PA10
 #define UART3_TX_PIN         PB10
 #define UART3_RX_PIN         PB11
 #define UART6_TX_PIN         PC6
 #define UART6_RX_PIN         PC7
+
+// I2C
+#define I2C1_SCL_PIN         PB8
+#define I2C1_SDA_PIN         PB9
+
+// ADC
 #define ADC_VBAT_PIN         PC3
-#define ESCSERIAL_PIN        PC7
+#define ADC_CURR_PIN         PC2 # --> pin AD
+#define ADC_RSSI_PIN         PA4 # --> pin DA
+#define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
+#define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
+
+// GYRO & ACC --> SPI1
+#define SPI1_SCK_PIN         PA5
+#define SPI1_SDI_PIN         PA6
+#define SPI1_SDO_PIN         PA7
+#define GYRO_1_CS_PIN        PC4
+#define GYRO_1_EXTI_PIN      PB15
+#define GYRO_1_SPI_INSTANCE  SPI1
+
+// ACC_SPI --> SPI2 (CS pin PB12)
+#define SPI2_SCK_PIN         PB13
+#define SPI2_SDI_PIN         PB14
+#define SPI2_SDO_PIN         PB15
+
+// SDCARD --> SPI3
+#define SPI3_SCK_PIN         PC10
+#define SPI3_SDI_PIN         PC11
+#define SPI3_SDO_PIN         PC12
 #define SDCARD_SPI_CS_PIN    PA15
 #define SDCARD_DETECT_PIN    PD2
+#define SDCARD_DETECT_INVERTED
+#define SDCARD_SPI_INSTANCE  SPI3
+#define DEFAULT_BLACKBOX_DEVICE     BLACKBOX_DEVICE_SDCARD
 
 #define TIMER_PIN_MAPPING \
     TIMER_PIN_MAP( 0, PA0 , 2,  0) \
@@ -78,13 +106,4 @@
     TIMER_PIN_MAP( 7, PC8 , 2, -1) \
     TIMER_PIN_MAP( 8, PC9 , 2, -1)
 
-
 #define ADC1_DMA_OPT        1
-
-#define DEFAULT_DSHOT_BURST DSHOT_DMAR_ON
-#define BEEPER_INVERTED
-#define GYRO_1_SPI_INSTANCE SPI1
-#define SDCARD_DETECT_INVERTED
-#define USE_SDCARD_SPI
-#define SDCARD_SPI_INSTANCE SPI3
-#define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
