3fa9d9cd4235bdbd8249836d59ee7eca3b54e3b5
H-ChuWu <754152299@qq.com>
2024-09-02 16:12:47 +0800

    修改

--- configs/BROTHERHOBBYF435/config.h
+++ configs/BROTHERHOBBYF435/config.h
@@ -27,36 +27,36 @@
 #define MANUFACTURER_ID   BROT
 
 // LEDS
-#define BEEPER_PIN             			 	PC15
-#define BEEPER_INVERTED				
-#define LED_STRIP_PIN          			 	PC9
-#define LED0_PIN               			 	PC13
+#define BEEPER_PIN                          PC15
+#define BEEPER_INVERTED			             
+#define LED_STRIP_PIN                       PC9
+#define LED0_PIN                            PC13
 #define LED0_INVERTED
 
 // MOTORS & SERVOS
-#define MOTOR1_PIN              			PA8
-#define MOTOR2_PIN              			PA9
-#define MOTOR3_PIN              			PB10
-#define MOTOR4_PIN              			PB11
-#define MOTOR5_PIN              			PB7
-#define MOTOR6_PIN              			PB6
-#define MOTOR7_PIN              			PB0
-#define MOTOR8_PIN              			PB1
+#define MOTOR1_PIN                          PA8
+#define MOTOR2_PIN                          PA9
+#define MOTOR3_PIN                          PB10
+#define MOTOR4_PIN                          PB11
+#define MOTOR5_PIN                          PB7
+#define MOTOR6_PIN                          PB6
+#define MOTOR7_PIN                          PB0
+#define MOTOR8_PIN                          PB1
+            
+// SPI          
+#define SPI1_SCK_PIN                        PA5
+#define SPI1_SDI_PIN                        PA6
+#define SPI1_SDO_PIN                        PA7
+            
+#define SPI2_SCK_PIN                        PB13
+#define SPI2_SDI_PIN                        PB14
+#define SPI2_SDO_PIN                        PB15
+            
+#define SPI3_SCK_PIN                        PB3
+#define SPI3_SDI_PIN                        PB4
+#define SPI3_SDO_PIN                        PB5
 
-// SPI			
-#define SPI1_SCK_PIN            			PA5
-#define SPI1_SDI_PIN            			PA6
-#define SPI1_SDO_PIN            			PA7
-
-#define SPI2_SCK_PIN            			PB13
-#define SPI2_SDI_PIN            			PB14
-#define SPI2_SDO_PIN            			PB15
-
-#define SPI3_SCK_PIN            			PB3
-#define SPI3_SDI_PIN            			PB4
-#define SPI3_SDO_PIN            			PB5
-
-// GYRO & ACC			
+// GYRO & ACC
 #define USE_ACC
 #define USE_GYRO
 #define USE_ACC_SPI_MPU6000
@@ -65,14 +65,14 @@
 #define USE_GYRO_SPI_ICM42688P
 #define USE_ACCGYRO_BMI270
 #define ENSURE_MPU_DATA_READY_IS_LOW
-#define GYRO_1_EXTI_PIN         			PC4
-#define GYRO_1_CS_PIN           			PA4
+#define GYRO_1_EXTI_PIN                     PC4
+#define GYRO_1_CS_PIN                       PA4
 #define GYRO_1_SPI_INSTANCE                 SPI1
 #define GYRO_1_ALIGN                        CW90_DEG
 
 // OSD
 #define USE_MAX7456
-#define MAX7456_SPI_CS_PIN      			PB12
+#define MAX7456_SPI_CS_PIN                  PB12
 #define MAX7456_SPI_INSTANCE                SPI2   
 
 // FLASH
@@ -80,7 +80,7 @@
 #define USE_FLASH_W25N
 #define USE_FLASH_W25Q128FV
 #define USE_FLASH_M25P16
-#define FLASH_CS_PIN            			PA15
+#define FLASH_CS_PIN                        PA15
 #define FLASH_SPI_INSTANCE                  SPI3
 #define DEFAULT_BLACKBOX_DEVICE             BLACKBOX_DEVICE_FLASH
 
@@ -94,45 +94,43 @@
 #define MAG_I2C_INSTANCE                    (I2CDEV_1)
 
 // UART & VCP
-#define UART1_RX_PIN            			PA10			
-#define UART2_TX_PIN            			PA2
-#define UART2_RX_PIN            			PA3
-#define UART3_TX_PIN            			PC10
-#define UART3_RX_PIN            			PC11
-#define UART4_TX_PIN            			PA0
-#define UART4_RX_PIN            			PA1
-#define UART5_TX_PIN            			PC12
-#define UART5_RX_PIN            			PD2
-#define UART6_TX_PIN            			PC6
-#define UART6_RX_PIN            			PC7
+#define UART1_RX_PIN                        PA10			
+#define UART2_TX_PIN                        PA2
+#define UART2_RX_PIN                        PA3
+#define UART3_TX_PIN                        PC10
+#define UART3_RX_PIN                        PC11
+#define UART4_TX_PIN                        PA0
+#define UART4_RX_PIN                        PA1
+#define UART5_TX_PIN                        PC12
+#define UART5_RX_PIN                        PD2
+#define UART6_TX_PIN                        PC6
+#define UART6_RX_PIN                        PC7
 
 // ADC			
-#define ADC_VBAT_PIN            			PC0
-#define ADC_RSSI_PIN            			PC5
-#define ADC_CURR_PIN            			PC1
-#define ADC_INSTANCE        				ADC1
-#define ADC1_DMA_OPT        				11
+#define ADC_VBAT_PIN                        PC0
+#define ADC_RSSI_PIN                        PC5
+#define ADC_CURR_PIN                        PC1
+#define ADC_INSTANCE                        ADC1
+#define ADC1_DMA_OPT                        11
 #define DEFAULT_CURRENT_METER_SOURCE        CURRENT_METER_ADC
 #define DEFAULT_VOLTAGE_METER_SOURCE        VOLTAGE_METER_ADC
-#define DEFAULT_CURRENT_METER_SCALE         386
 
 // PINIO
-#define PINIO1_PIN 							PC14
-#define PINIO1_CONFIG 						129
-#define PINIO1_BOX 							40
+#define PINIO1_PIN                          PC14
+#define PINIO1_CONFIG                       129
+#define PINIO1_BOX                          40
 
 // TIMERS & DMA
 #define TIMER_PIN_MAPPING \
- 	TIMER_PIN_MAP(0, PA8, 1, 7) \
- 	TIMER_PIN_MAP(1, PA9, 1, 1) \
- 	TIMER_PIN_MAP(2, PB10, 1, 13) \
- 	TIMER_PIN_MAP(3, PB11, 1, 12) \
- 	TIMER_PIN_MAP(4, PB7, 1, 4) \
- 	TIMER_PIN_MAP(5, PB6, 1, 2) \
- 	TIMER_PIN_MAP(6, PB0, 2, 9) \
- 	TIMER_PIN_MAP(7, PB1, 2, 8) \
- 	TIMER_PIN_MAP(8, PC9, 2, 3)
+ 	TIMER_PIN_MAP(0, PA8, 1, 0) \
+ 	TIMER_PIN_MAP(1, PA9, 1, 0) \
+ 	TIMER_PIN_MAP(2, PB10, 1, 0) \
+ 	TIMER_PIN_MAP(3, PB11, 1, 0) \
+ 	TIMER_PIN_MAP(4, PB7, 1, 0) \
+ 	TIMER_PIN_MAP(5, PB6, 1, 0) \
+ 	TIMER_PIN_MAP(6, PB0, 2, 0) \
+ 	TIMER_PIN_MAP(7, PB1, 2, 0) \
+ 	TIMER_PIN_MAP(8, PC9, 2, 0)
 
 #define DEFAULT_DSHOT_BURST                 DSHOT_DMAR_AUTO
-#define DEFAULT_DSHOT_BITBANG          		DSHOT_BITBANG_ON
-#define DEFAULT_FEATURES (FEATURE_OSD | FEATURE_LED_STRIP)
+#define DEFAULT_DSHOT_BITBANG               DSHOT_BITBANG_ON
