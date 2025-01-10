e368737c092d97a083a43aa580174910c4a9655c
ot0tot <36753790+ot0tot@users.noreply.github.com>
2025-01-09 23:03:17 -0500

    Update config.h based on revised schematic

--- configs/JHEG473/config.h
+++ configs/JHEG473/config.h
@@ -30,19 +30,17 @@
 #define USE_GYRO
 #define USE_ACC_SPI_ICM42688P
 #define USE_GYRO_SPI_ICM42688P
-#define USE_ACCGYRO_BMI270
 #define USE_BARO
-#define USE_BARO_BMP280
 #define USE_BARO_DPS310
 #define USE_FLASH
-#define USE_FLASH_M25P16
+#define USE_FLASH_W25Q128FV
 #define USE_MAX7456
 
 #define BEEPER_PIN          PA8
 #define MOTOR1_PIN          PB0
 #define MOTOR2_PIN          PB1
-#define MOTOR3_PIN          PB6
-#define MOTOR4_PIN          PC13
+#define MOTOR3_PIN          PA4
+#define MOTOR4_PIN          PC6
 #define LED_STRIP_PIN       PB2
 #define UART1_TX_PIN        PA9
 #define UART1_RX_PIN        PA10
@@ -54,9 +52,9 @@
 #define UART4_RX_PIN        PC11
 #define I2C1_SCL_PIN        PA15
 #define I2C1_SDA_PIN        PB7
-#define LED0_PIN            PC15
-#define LED1_PIN            PC14
-#define PINIO1_PIN          PC6
+#define LED0_PIN            PB6
+#define LED1_PIN            PB9
+#define PINIO1_PIN          PC15
 #define SPI1_SCK_PIN        PA5
 #define SPI1_SDI_PIN        PA6
 #define SPI1_SDO_PIN        PA7
@@ -68,22 +66,20 @@
 #define SPI3_SDO_PIN        PB5
 #define ADC_VBAT_PIN        PA0
 #define ADC_CURR_PIN        PA1
-#define FLASH_CS_PIN        PB9
+#define FLASH_CS_PIN        PC14
 #define MAX7456_SPI_CS_PIN  PB12
 #define GYRO_1_EXTI_PIN     PC4
-#define GYRO_1_CS_PIN       PA4
+#define GYRO_1_CS_PIN       PC13
 
 #define TIMER_PIN_MAPPING \
-    TIMER_PIN_MAP( 0, LED_STRIP_PIN, 1, 0 ) \
-    TIMER_PIN_MAP( 1, MOTOR1_PIN,    1, 1 ) \
-    TIMER_PIN_MAP( 2, MOTOR2_PIN,    1, 2 ) \
-    TIMER_PIN_MAP( 3, MOTOR3_PIN,    3, 3 ) \
-    TIMER_PIN_MAP( 4, MOTOR4_PIN,    2, 4 )
+    TIMER_PIN_MAP( 0, LED_STRIP_PIN, 1,  0 ) \
+    TIMER_PIN_MAP( 1, MOTOR1_PIN,    1,  1 ) \
+    TIMER_PIN_MAP( 2, MOTOR2_PIN,    1,  2 ) \
+    TIMER_PIN_MAP( 3, MOTOR3_PIN,    1,  3 ) \
+    TIMER_PIN_MAP( 4, MOTOR4_PIN,    1,  4 ) \
+    TIMER_PIN_MAP( 5, BEEPER_PIN,    1, -1 )
 
 #define ADC1_DMA_OPT 8
-
-#define VTX_SMARTAUDIO_UART             SERIAL_PORT_USART2
-#define SERIALRX_UART                   SERIAL_PORT_USART3
 
 #define BARO_I2C_INSTANCE               I2CDEV_1
 #define MAG_I2C_INSTANCE                I2CDEV_1
@@ -93,7 +89,6 @@
 #define DEFAULT_DSHOT_BITBANG           DSHOT_BITBANG_ON
 #define DEFAULT_CURRENT_METER_SOURCE    CURRENT_METER_ADC
 #define DEFAULT_VOLTAGE_METER_SOURCE    VOLTAGE_METER_ADC
-#define DEFAULT_CURRENT_METER_SCALE     750
 #define BEEPER_INVERTED
 #define MAX7456_SPI_INSTANCE            SPI2
 #define FLASH_SPI_INSTANCE              SPI3
