58d3eacb2f5ad0ad22c43631843edc96a198e0cc
vkirovskiy <vkirovskiy@gmail.com>
2025-01-18 23:37:22 +0200

    Add MATEKF405WINGV2 target

--- configs/MATEKF405WINGV2/config.h
+++ configs/MATEKF405WINGV2/config.h
@@ -27,10 +27,13 @@
 #define MANUFACTURER_ID   MTKS
 
 #define USE_ACC
+#define USE_ACC_SPI_MPU6000
 #define USE_ACC_SPI_ICM42688P
 #define USE_GYRO
+#define USE_GYRO_SPI_MPU6000
 #define USE_GYRO_SPI_ICM42688P
 #define USE_BARO
+#define USE_BARO_BMP280
 #define USE_BARO_DPS310
 #define USE_MAX7456
 #define USE_SDCARD
@@ -44,14 +47,15 @@
 #endif
 
 #define BEEPER_PIN           PC15
-#define MOTOR1_PIN           PB7
-#define MOTOR2_PIN           PB6
-#define SERVO1_PIN           PB0
-#define SERVO2_PIN           PB1
-#define SERVO3_PIN           PC8
-#define SERVO4_PIN           PC9
-#define SERVO5_PIN           PB14
-#define SERVO6_PIN           PB15
+#define MOTOR1_PIN           PB7   //S1
+#define MOTOR2_PIN           PB6   //S2
+#define SERVO1_PIN           PB0   //S3
+#define SERVO2_PIN           PB1   //S4
+#define SERVO3_PIN           PC8   //S5
+#define SERVO4_PIN           PC9   //S6
+#define SERVO5_PIN           PB14  //S7
+#define SERVO6_PIN           PB15  //S8
+#define SERVO7_PIN           PA8   //S9
 #define RX_PPM_PIN           PA3
 #define LED_STRIP_PIN        PA15
 #define UART1_TX_PIN         PA9
@@ -93,16 +97,14 @@
 #define TIMER_PIN_MAPPING \
     TIMER_PIN_MAP( 0, PB7 , 1,  0) \
     TIMER_PIN_MAP( 1, PB6 , 1,  0) \
-    TIMER_PIN_MAP( 2, PB0 , 2,  0) \
-    TIMER_PIN_MAP( 3, PB1 , 2,  0) \
-    TIMER_PIN_MAP( 4, PC8 , 2,  1) \
-    TIMER_PIN_MAP( 5, PC9 , 2,  0) \
+    TIMER_PIN_MAP( 2, PB0 , 2,  -1) \
+    TIMER_PIN_MAP( 3, PB1 , 2,  -1) \
+    TIMER_PIN_MAP( 4, PC8 , 2,  -1) \
+    TIMER_PIN_MAP( 5, PC9 , 2,  -1) \
     TIMER_PIN_MAP( 6, PB14, 3, -1) \
     TIMER_PIN_MAP( 7, PB15, 3, -1) \
-    TIMER_PIN_MAP( 8, PA8 , 1,  0) \
-    TIMER_PIN_MAP( 9, PA15, 1,  0) \
-    TIMER_PIN_MAP(10, PA3 , 3, -1) \
-    TIMER_PIN_MAP(11, PA2 , 2,  0)
+    TIMER_PIN_MAP( 8, PA3 , 3, -1) \
+    TIMER_PIN_MAP( 9, PA8 , 1,  1)
 
 
 #define SPI3_TX_DMA_OPT     0
@@ -113,6 +115,7 @@
 #define BARO_I2C_INSTANCE I2CDEV_1
 #define DEFAULT_BLACKBOX_DEVICE     BLACKBOX_DEVICE_SDCARD
 #define DEFAULT_DSHOT_BURST DSHOT_DMAR_OFF
+#define DEFAULT_DSHOT_BITBANG DSHOT_BITBANG_OFF
 #define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
 #define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
 #define DEFAULT_CURRENT_METER_SCALE 150
