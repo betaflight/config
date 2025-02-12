585a2458ab205b409ea67388d5a88b62223e0be5
mygodamn <winchester.mygod@hotmail.com>
2025-02-12 23:55:40 +0800

    Update configs/NBD_HUMMINGBIRD200RS/config.h
    remove pre defined baro and mag cause it's not onboard device
    
    Co-authored-by: ot0tot <36753790+ot0tot@users.noreply.github.com>

--- configs/NBD_HUMMINGBIRD200RS/config.h
+++ configs/NBD_HUMMINGBIRD200RS/config.h
@@ -31,10 +31,6 @@
 #define USE_GYRO
 #define USE_GYRO_SPI_ICM42688P
 
-#define USE_BARO
-#define USE_BARO_DPS310
-#define USE_MAG
-#define USE_MAG_QMC5883
 #define USE_FLASH
 #define USE_FLASH_M25P16
 #define USE_MAX7456
