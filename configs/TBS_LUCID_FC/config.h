e1114f35a30d4e43b3fae32ed38b58e3ae05b049
TheIsotopes <theisotopes@byom.de>
2025-02-16 14:48:11 +0100

    Tbs lucid fc fixes (#686)
    * cleanup
    
    * fix board orientation on pro fc
    
    is not used imo, but should still be fixed if someone uses it.

--- configs/TBS_LUCID_FC/config.h
+++ configs/TBS_LUCID_FC/config.h
@@ -33,7 +33,6 @@
 #define USE_GYRO
 #define USE_GYRO_SPI_MPU6000
 #define USE_GYRO_SPI_ICM42688P
-#define USE_SPI_GYRO
 
 #define GYRO_1_EXTI_PIN                     PC4
 #define GYRO_1_CS_PIN                       PA4
@@ -132,7 +131,7 @@
 
 #define ADC_INSTANCE                        ADC1
 #define BARO_I2C_INSTANCE                   I2CDEV_1
-#define MAG_I2C_INSTANCE                            I2CDEV_1
+#define MAG_I2C_INSTANCE                    I2CDEV_1
 
 #define DEFAULT_BLACKBOX_DEVICE             BLACKBOX_DEVICE_FLASH
 
