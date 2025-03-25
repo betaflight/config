7beff14306b953d5a567389706d1d7caac53db44
tobeornottobe81 <57481312+tobeornottobe81@users.noreply.github.com>
2025-03-25 17:05:45 +0800

    Update config.h
    modify FLYCOLORH743 defines

--- configs/FLYCOLORH743/config.h
+++ configs/FLYCOLORH743/config.h
@@ -27,10 +27,8 @@
 #define MANUFACTURER_ID   FLCO
 
 #define USE_GYRO
-#define USE_GYRO_SPI_MPU6000
 #define USE_GYRO_SPI_ICM42688P
 #define USE_ACC
-#define USE_ACC_SPI_MPU6000
 #define USE_ACC_SPI_ICM42688P
 #define USE_BARO
 #define USE_BARO_BMP388
@@ -74,21 +72,16 @@
 #define SPI1_SCK_PIN         PA5
 #define SPI2_SCK_PIN         PB13
 #define SPI3_SCK_PIN         PC10
-#define SPI4_SCK_PIN         PE12
 #define SPI1_SDI_PIN         PA6
 #define SPI2_SDI_PIN         PB14
 #define SPI3_SDI_PIN         PC11
-#define SPI4_SDI_PIN         PE13
 #define SPI1_SDO_PIN         PA7
 #define SPI2_SDO_PIN         PB15
 #define SPI3_SDO_PIN         PB2
-#define SPI4_SDO_PIN         PE14
 #define FLASH_CS_PIN         PA15
 #define MAX7456_SPI_CS_PIN   PB12
 #define GYRO_1_EXTI_PIN      PC8
-#define GYRO_2_EXTI_PIN      PE15
 #define GYRO_1_CS_PIN        PA4
-#define GYRO_2_CS_PIN        PE11
 #define ADC_VBAT_PIN         PC1
 #define ADC_RSSI_PIN         PC2
 #define ADC_CURR_PIN         PC3
@@ -118,7 +111,6 @@
 #define TIMUP5_DMA_OPT      0
 
 #define SERIALRX_UART SERIAL_PORT_UART4
-#define USE_BARO
 #define BARO_I2C_INSTANCE I2CDEV_1
 #define MAG_I2C_INSTANCE I2CDEV_2
 #define FLASH_SPI_INSTANCE SPI3
@@ -137,5 +129,3 @@
 #define PINIO2_BOX 41
 #define GYRO_1_SPI_INSTANCE SPI1
 #define GYRO_1_ALIGN CW0_DEG
-#define GYRO_2_SPI_INSTANCE SPI4
-#define GYRO_2_ALIGN CW0_DEG
