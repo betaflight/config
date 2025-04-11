b3fce472d3265f4baa7c7d29c678bfddd816e1b1
ot0tot <36753790+ot0tot@users.noreply.github.com>
2025-04-11 16:22:37 -0400

    Fix I2C

--- configs/SKYSTARSF435PRO/config.h
+++ configs/SKYSTARSF435PRO/config.h
@@ -62,8 +62,8 @@
 #define UART6_RX_PIN         PC7
 #define I2C1_SCL_PIN         PB8
 #define I2C1_SDA_PIN         PB9
-#define I2C1_SCL_PIN         PH2
-#define I2C1_SDA_PIN         PH3
+#define I2C2_SCL_PIN         PH2
+#define I2C2_SDA_PIN         PH3
 #define LED0_PIN             PC3
 #define SPI1_SCK_PIN         PA5
 #define SPI2_SCK_PIN         PB13
