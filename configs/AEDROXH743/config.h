0519c67a04955f3ef5c8a7643b514a50e788b501
Mark Haslinghuis <mark@numloq.nl>
2025-04-16 22:11:25 +0200

    Update configs/AEDROXH743/config.h

--- configs/AEDROXH743/config.h
+++ configs/AEDROXH743/config.h
@@ -151,7 +151,7 @@
                                         TIMER_PIN_MAP( 7, MOTOR8_PIN,       1,  7 ) \
                                         TIMER_PIN_MAP( 8, LED_STRIP_PIN,    1,  9 ) \
                                         TIMER_PIN_MAP( 9, GYRO_1_CLKIN_PIN, 1, -1 ) \
-                                        TIMER_PIN_MAP( 10, BEEPER_PIN,      2, -1 )
+                                        TIMER_PIN_MAP(10, BEEPER_PIN,       2, -1 )
 
 #define ADC1_DMA_OPT                    8
 #define ADC3_DMA_OPT                    10
