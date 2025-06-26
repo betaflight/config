e0b53356480d3d7a279bda27ecca3c7cdb54d876
LYNHQQ <89908604+LYNHQQ@users.noreply.github.com>
2025-06-26 10:22:15 +0800

    Update configs/HUMMINGBIRD_FC305/config.h
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>

--- configs/HUMMINGBIRD_FC305/config.h
+++ configs/HUMMINGBIRD_FC305/config.h
@@ -82,8 +82,8 @@
 
 #define TIMER_PIN_MAPPING \
     TIMER_PIN_MAP( 0, LED_STRIP_PIN, 1, 0) \
-    TIMER_PIN_MAP( 1, MOTOR1_PIN   , 1, 0 ) \
-    TIMER_PIN_MAP( 2, MOTOR2_PIN   , 2, 0 ) \
+    TIMER_PIN_MAP( 1, MOTOR1_PIN   , 2, 0 ) \
+    TIMER_PIN_MAP( 2, MOTOR2_PIN   , 1, 0 ) \
     TIMER_PIN_MAP( 3, MOTOR3_PIN   , 1, 0 ) \
     TIMER_PIN_MAP( 4, MOTOR4_PIN   , 2, 0 ) \
     TIMER_PIN_MAP( 5, GYRO_1_CLKIN_PIN , 1,  -1) 
