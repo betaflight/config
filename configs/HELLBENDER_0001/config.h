a6bd6a757a7fb9d6d468e13c0a0bf26b7f4929c9
blckmn <blckmn@betaflight.com>
2025-08-11 09:51:22 +1000

    Formatting

--- configs/HELLBENDER_0001/config.h
+++ configs/HELLBENDER_0001/config.h
@@ -78,9 +78,9 @@
 #define PICO_BEC_5V_ENABLE_PIN PA14
 #define PICO_BEC_9V_ENABLE_PIN PA15
 
-#define ADC_VBAT_PIN PA40
-#define ADC_CURR_PIN PA41
-#define ADC_RSSI_PIN PA42
+#define ADC_VBAT_PIN         PA40
+#define ADC_CURR_PIN         PA41
+#define ADC_RSSI_PIN         PA42
 
 /*
 
