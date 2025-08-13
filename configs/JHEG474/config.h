19a8059b10914ba5d36adcf5cd37c65f8e0bbe17
Mark Haslinghuis <mark@numloq.nl>
2025-08-13 21:35:17 +0200

    Fix manufacturer ID
    - fixes #860

--- configs/JHEG474/config.h
+++ configs/JHEG474/config.h
@@ -24,7 +24,7 @@
 #define FC_TARGET_MCU       STM32G47X
 
 #define BOARD_NAME          JHEG474
-#define MANUFACTURER_ID     JHEM
+#define MANUFACTURER_ID     JHEF
 
 #define USE_ACC
 #define USE_GYRO
