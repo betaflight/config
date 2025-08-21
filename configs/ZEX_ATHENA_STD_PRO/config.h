2d14356e1699edcf3428e6613aed04686f8f71cf
Mark Haslinghuis <mark@numloq.nl>
2025-08-21 15:54:38 +0200

    Remove beeper from timers to avoid conflict with ledstrip

--- configs/ZEX_ATHENA_STD_PRO/config.h
+++ configs/ZEX_ATHENA_STD_PRO/config.h
@@ -105,9 +105,8 @@
     TIMER_PIN_MAP( 8,  PE5 , 1, -1) /* TIM15 CH1 (AF4) DMA1 Stream 1 Request 105 */ \
     TIMER_PIN_MAP( 9,  PE6 , 1, -1) /* TIM15 CH2 (AF4)                           */ \
     TIMER_PIN_MAP(10,  PA8 , 1,  8) /* TIM1  CH1 (AF1) DMA1 Stream 1 Request  11 */ \
-    TIMER_PIN_MAP(11,  PE9 , 1, -1) /* TIM1  CH1 (AF1)                           */ \
-    TIMER_PIN_MAP(12,  PA15, 1, -1) /* TIM2  CH1 (AF1)                           */ \
-    TIMER_PIN_MAP(13,  PB0 , 1, -1) /* TIM3  CH3 (AF2)                           */
+    TIMER_PIN_MAP(11,  PA15, 1, -1) /* TIM2  CH1 (AF1)                           */ \
+    TIMER_PIN_MAP(12,  PB0 , 1, -1) /* TIM3  CH3 (AF2)                           */
 
 #define ADC1_DMA_OPT                    9 /*# ADC 1: DMA2 Stream 0 Request 9*/
 #define ADC3_DMA_OPT                    10 /*# ADC 3: DMA2 Stream 1 Request 115*/
