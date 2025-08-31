2ccf29a0054ee2f3e6a4c9108c590e259b664300
Mark Haslinghuis <mark@numloq.nl>
2025-09-01 00:03:52 +0200

    Update DMA TIMUP

--- configs/GEPRCTAKER_H743_MINI/config.h
+++ configs/GEPRCTAKER_H743_MINI/config.h
@@ -102,9 +102,8 @@
 
 #define ADC1_DMA_OPT                    9
 #define ADC3_DMA_OPT                    10
-#define TIMUP3_DMA_OPT                  11
-#define TIMUP4_DMA_OPT                  12
-#define TIMUP8_DMA_OPT                  13
+#define TIMUP1_DMA_OPT                  11
+#define TIMUP8_DMA_OPT                  12
 
 #define MSP_DISPLAYPORT_UART            SERIAL_PORT_USART1
 #define SERIALRX_UART                   SERIAL_PORT_USART2
