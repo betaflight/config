98256ecb379b37bb5f500f6c44827a3d115cb529
Mark Haslinghuis <mark@numloq.nl>
2025-09-01 00:04:50 +0200

    Fix TIMER_PIN_MAPPING for LED_STRIP_PIN

--- configs/GEPRCTAKER_H743_MINI/config.h
+++ configs/GEPRCTAKER_H743_MINI/config.h
@@ -89,7 +89,7 @@
 
 
 #define TIMER_PIN_MAPPING \
-    TIMER_PIN_MAP( 0, LED_STRIP_PIN, 1, 14) \
+    TIMER_PIN_MAP( 0, LED_STRIP_PIN, 1, 0) \
     TIMER_PIN_MAP( 1, MOTOR1_PIN,    2, 1) \
     TIMER_PIN_MAP( 2, MOTOR2_PIN,    2, 2) \
     TIMER_PIN_MAP( 3, MOTOR3_PIN,    2, 3) \
