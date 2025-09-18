d6a23c90c1fbf837f954a7e458f0a8fa931f81e3
Mark Haslinghuis <mark@numloq.nl>
2025-09-18 12:51:11 +0200

    Update pins according new schematic

--- configs/JHEF722PRO/config.h
+++ configs/JHEF722PRO/config.h
@@ -38,11 +38,11 @@
 #define USE_MAX7456
 
 #define BEEPER_PIN           PC15
-#define MOTOR1_PIN           PB0
-#define MOTOR2_PIN           PB1
-#define MOTOR3_PIN           PB4
-#define MOTOR4_PIN           PB3
-#define SERVO1_PIN           PC8
+#define MOTOR1_PIN           PC6
+#define MOTOR2_PIN           PC7
+#define MOTOR3_PIN           PC8
+#define MOTOR4_PIN           PC9
+#define SERVO1_PIN           PB0
 #define RX_PPM_PIN           PA3
 #define LED_STRIP_PIN        PA8
 #define UART1_TX_PIN         PA9
@@ -50,13 +50,11 @@
 #define UART3_TX_PIN         PB10
 #define UART4_TX_PIN         PA0
 #define UART5_TX_PIN         PC12
-#define UART6_TX_PIN         PC6
 #define UART1_RX_PIN         PA10
 #define UART2_RX_PIN         PA3
 #define UART3_RX_PIN         PB11
 #define UART4_RX_PIN         PA1
 #define UART5_RX_PIN         PD2
-#define UART6_RX_PIN         PC7
 #define I2C1_SCL_PIN         PB6
 #define I2C1_SDA_PIN         PB7
 #define LED0_PIN             PA15
@@ -83,12 +81,12 @@
 #define TIMER_PIN_MAPPING \
     TIMER_PIN_MAP( 0, PA3, 3, -1 ) \
     TIMER_PIN_MAP( 1, PA8, 1,  0 ) \
-    TIMER_PIN_MAP( 2, PB0, 2,  0 ) \
-    TIMER_PIN_MAP( 3, PB1, 2,  0 ) \
-    TIMER_PIN_MAP( 4, PB4, 1,  0 ) \
-    TIMER_PIN_MAP( 5, PB3, 1,  0 ) \
+    TIMER_PIN_MAP( 2, PC6, 2,  0 ) \
+    TIMER_PIN_MAP( 3, PC7, 2,  0 ) \
+    TIMER_PIN_MAP( 4, PC8, 2,  0 ) \
+    TIMER_PIN_MAP( 5, PC9, 2,  0 ) \
     TIMER_PIN_MAP( 6, PB8, 1, -1 ) \
-    TIMER_PIN_MAP( 7, PC8, 2, -1 )
+    TIMER_PIN_MAP( 7, PB0, 2, -1 )
 
 #define ADC3_DMA_OPT                    1
 
@@ -103,6 +101,9 @@
 #define BEEPER_INVERTED
 #define PINIO1_BOX                      40
 #define PINIO2_BOX                      41
+#define PINIO1_CONFIG                   129
+#define BOX_USER1_NAME                  "10V BEC"
+#define BOX_USER2_NAME                  "COB"
 #define MAX7456_SPI_INSTANCE            SPI2
 #define FLASH_SPI_INSTANCE              SPI3
 #define GYRO_1_SPI_INSTANCE             SPI1
