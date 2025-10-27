6eea2791c7124f262be69fc14f4bc607360d7021
Mark Haslinghuis <mark@numloq.nl>
2025-10-27 12:44:15 +0100

    Swap TIMER_PIN_MAPPING for PA2 and PA3

--- configs/JHEF435PRO/config.h
+++ configs/JHEF435PRO/config.h
@@ -77,8 +77,8 @@
 #define TIMER_PIN_MAPPING   TIMER_PIN_MAP(0, PB6, 1,  0) \
                             TIMER_PIN_MAP(1, PA0, 1,  1) \
                             TIMER_PIN_MAP(2, PA1, 1,  2) \
-                            TIMER_PIN_MAP(3, PA2, 1,  3) \
-                            TIMER_PIN_MAP(4, PA3, 1,  4) \
+                            TIMER_PIN_MAP(3, PA3, 1,  3) \
+                            TIMER_PIN_MAP(4, PA2, 1,  4) \
                             TIMER_PIN_MAP(5, PA8, 1, -1)
 
 #define ADC_INSTANCE                    ADC1
