96002a10f8c70ab0a31691048dcd4cf0b791d30b
ot0tot <36753790+ot0tot@users.noreply.github.com>
2025-12-05 11:31:05 -0500

    Add GEPRC_TAKER_F405AIO config.h (#951)
    * Add GEPRC_TAKER_F405AIO config.h
    
    * Fix gyro alignment
    
    * Add default on-board receiver

---
+++ configs/GEPRC_TAKER_F405AIO/config.h
@@ -0,0 +1,116 @@
+/*
+* This file is part of Betaflight.
+*
+* Betaflight is free software. You can redistribute this software
+* and/or modify this software under the terms of the GNU General
+* Public License as published by the Free Software Foundation,
+* either version 3 of the License, or (at your option) any later
+* version.
+*
+* Betaflight is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public
+* License along with this software.
+*
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define FC_TARGET_MCU     STM32F405
+
+#define BOARD_NAME        GEPRC_TAKER_F405AIO
+#define MANUFACTURER_ID   GEPR
+
+#define USE_ACC
+#define USE_ACC_SPI_ICM42688P
+#define USE_GYRO
+#define USE_GYRO_SPI_ICM42688P
+#define USE_BARO
+#define USE_BARO_DPS310
+#define USE_FLASH
+#define USE_FLASH_W25Q128FV
+#define USE_MAX7456
+#define USE_GYRO_CLKIN
+
+#define BEEPER_PIN           PD7
+#define MOTOR1_PIN           PE9
+#define MOTOR2_PIN           PE11
+#define MOTOR3_PIN           PE13
+#define MOTOR4_PIN           PE14
+#define RX_PPM_PIN           PA3
+#define LED_STRIP_PIN        PD14
+#define UART1_TX_PIN         PA9
+#define UART2_TX_PIN         PD5
+#define UART3_TX_PIN         PC10
+#define UART4_TX_PIN         PA0
+#define UART5_TX_PIN         PC12
+#define UART6_TX_PIN         PC6
+#define UART1_RX_PIN         PA10
+#define UART2_RX_PIN         PD6
+#define UART3_RX_PIN         PC11
+#define UART4_RX_PIN         PA1
+#define UART5_RX_PIN         PD2
+#define UART6_RX_PIN         PC7
+#define I2C1_SCL_PIN         PB8
+#define I2C2_SCL_PIN         PB10
+#define I2C1_SDA_PIN         PB9
+#define I2C2_SDA_PIN         PB11
+#define LED0_PIN             PD15
+#define SPI1_SCK_PIN         PA5
+#define SPI2_SCK_PIN         PB13
+#define SPI3_SCK_PIN         PB3
+#define SPI1_SDI_PIN         PA6
+#define SPI2_SDI_PIN         PB14
+#define SPI3_SDI_PIN         PB4
+#define SPI1_SDO_PIN         PA7
+#define SPI2_SDO_PIN         PB15
+#define SPI3_SDO_PIN         PB5
+#define ADC_VBAT_PIN         PC3
+#define ADC_CURR_PIN         PC2
+#define PINIO1_PIN           PC14 // USER1
+#define PINIO2_PIN           PC13 // VTX-SW
+#define FLASH_CS_PIN         PB6
+#define MAX7456_SPI_CS_PIN   PB12
+#define GYRO_1_EXTI_PIN      PB0
+#define GYRO_1_CS_PIN        PA4
+#define GYRO_1_CLKIN_PIN     PB1
+#define USB_DETECT_PIN       PE8
+
+#define TIMER_PIN_MAPPING \
+TIMER_PIN_MAP( 0, PE9 , 1,  2) \
+TIMER_PIN_MAP( 1, PE11, 1,  1) \
+TIMER_PIN_MAP( 2, PE13, 1,  1) \
+TIMER_PIN_MAP( 3, PE14, 1,  0) \
+TIMER_PIN_MAP( 4, PA3 , 3, -1) \
+TIMER_PIN_MAP( 5, PD14, 1,  0) \
+TIMER_PIN_MAP( 6, PB1 , 2, -1) 
+
+
+#define ADC3_DMA_OPT        1
+#define ADC_INSTANCE        ADC3
+#define BARO_I2C_INSTANCE   I2CDEV_1
+#define MAG_I2C_INSTANCE    I2CDEV_2
+#define PINIO1_BOX          40
+#define PINIO1_CONFIG       1
+#define PINIO2_BOX          41
+#define PINIO2_CONFIG       129
+#define BOX_USER2_NAME      "VTX-SW"
+
+#define SERIALRX_UART                SERIAL_PORT_USART2 // Built-in ELRS receiver on UART2
+#define SERIALRX_PROVIDER            SERIALRX_CRSF
+
+#define DEFAULT_BLACKBOX_DEVICE      BLACKBOX_DEVICE_FLASH
+#define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
+#define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
+#define DEFAULT_DSHOT_BITBANG        DSHOT_BITBANG_OFF
+#define BEEPER_INVERTED
+#define SYSTEM_HSE_MHZ               8
+#define MAX7456_SPI_INSTANCE         SPI2
+#define FLASH_SPI_INSTANCE           SPI3
+#define GYRO_1_SPI_INSTANCE          SPI1
+#define DEFAULT_ALIGN_BOARD_YAW      45
