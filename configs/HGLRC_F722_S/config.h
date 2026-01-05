398fe71bd63858d89717eae78223c1d5226bef0b
HGLRC-T <technical@hglrc.com>
2026-01-05 19:57:56 +0800

    Added support for MAG, LEDSTRIP, and GPS.

--- configs/HGLRC_F722_S/config.h
+++ configs/HGLRC_F722_S/config.h
@@ -92,6 +92,18 @@
     TIMER_PIN_MAP( 4, LED_STRIP_PIN, 1, 0) \
     TIMER_PIN_MAP( 5, GYRO_1_CLKIN_PIN, 1, 0)
 
+#ifndef USE_LED_STRIP
+#define USE_LED_STRIP
+#endif
+
+#ifndef USE_GPS
+#define USE_GPS
+#endif
+
+#ifndef USE_MAG
+#define USE_MAG
+#endif
+
 #define MAG_I2C_INSTANCE  I2CDEV_1
 #define BARO_I2C_INSTANCE I2CDEV_1
 
@@ -103,5 +115,5 @@
 #define DEFAULT_CURRENT_METER_SCALE  165    
 
 #define SERIALRX_UART        SERIAL_PORT_USART2
-#define ESC_SENSOR_UART      SERIAL_PORT_UART3
-#define GPS_UART             SERIAL_PORT_UART1
+#define ESC_SENSOR_UART      SERIAL_PORT_USART3
+#define GPS_UART             SERIAL_PORT_USART1
