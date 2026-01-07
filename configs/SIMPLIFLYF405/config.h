e38f1aa59d5d5c5256909ef6b97387442753fc8f
Mark Haslinghuis <mark@numloq.nl>
2026-01-07 17:20:14 +0100

    Refactor

--- configs/SIMPLIFLYF405/config.h
+++ configs/SIMPLIFLYF405/config.h
@@ -36,6 +36,7 @@
 #define USE_FLASH
 #define USE_FLASH_W25Q128FV
 
+#define BEEPER_INVERTED
 #define BEEPER_PIN           PB1
 
 #define MOTOR1_PIN           PC6
@@ -58,22 +59,37 @@
 #define UART4_RX_PIN         PA1
 #define UART5_RX_PIN         PD2
 
+// I2C1 for Mag/Baro
 #define I2C1_SCL_PIN         PB8
 #define I2C1_SDA_PIN         PB9
+#define MAG_I2C_INSTANCE     I2CDEV_1
+#define BARO_I2C_INSTANCE    I2CDEV_1
 
 #define LED0_PIN             PB0
 
+// SPI1 for Gyro/Acc
 #define SPI1_SCK_PIN         PA5
-#define SPI2_SCK_PIN         PB13
-#define SPI3_SCK_PIN         PC10
-
 #define SPI1_SDI_PIN         PA6
-#define SPI2_SDI_PIN         PB14
-#define SPI3_SDI_PIN         PC11
-
 #define SPI1_SDO_PIN         PA7
+#define GYRO_1_EXTI_PIN      PC5
+#define GYRO_1_CS_PIN        PC4
+#define GYRO_1_SPI_INSTANCE  SPI1
+#define GYRO_1_ALIGN         CW270_DEG_FLIP
+
+// SPI2 for Max7456 OSD
+#define SPI2_SCK_PIN         PB13
+#define SPI2_SDI_PIN         PB14
 #define SPI2_SDO_PIN         PB15
+#define MAX7456_SPI_CS_PIN   PB12
+#define MAX7456_SPI_INSTANCE SPI2
+#define DEFAULT_BLACKBOX_DEVICE BLACKBOX_DEVICE_FLASH
+
+// SPI3 for Flash
+#define SPI3_SCK_PIN         PC10
+#define SPI3_SDI_PIN         PC11
 #define SPI3_SDO_PIN         PB5
+#define FLASH_CS_PIN         PB7
+#define FLASH_SPI_INSTANCE   SPI3
 
 #define CAMERA_CONTROL_PIN   PB3
 
@@ -81,40 +97,23 @@
 #define ADC_RSSI_PIN         PC0
 #define ADC_CURR_PIN         PC1
 
+// PinIO
 #define PINIO1_PIN           PB2
-
-#define FLASH_CS_PIN         PB7
-#define MAX7456_SPI_CS_PIN   PB12
-#define GYRO_1_EXTI_PIN      PC5
-#define GYRO_1_CS_PIN        PC4
-
+#define PINIO1_CONFIG        129
+#define PINIO1_BOX           0
 
 #define TIMER_PIN_MAPPING \
-    TIMER_PIN_MAP( 0, MOTOR1_PIN , 2,  0) \
-    TIMER_PIN_MAP( 1, MOTOR2_PIN , 2,  0) \
-    TIMER_PIN_MAP( 2, MOTOR3_PIN , 2,  0) \
-    TIMER_PIN_MAP( 3, MOTOR4_PIN , 2,  0) \
-    TIMER_PIN_MAP( 4, LED_STRIP_PIN , 1,  0) \
-    TIMER_PIN_MAP(5, CAMERA_CONTROL_PIN , 2, -1)
+    TIMER_PIN_MAP( 0, MOTOR1_PIN , 2,  0 ) \
+    TIMER_PIN_MAP( 1, MOTOR2_PIN , 2,  0 ) \
+    TIMER_PIN_MAP( 2, MOTOR3_PIN , 2,  0 ) \
+    TIMER_PIN_MAP( 3, MOTOR4_PIN , 2,  0 ) \
+    TIMER_PIN_MAP( 4, LED_STRIP_PIN , 1,  0 ) \
+    TIMER_PIN_MAP( 5, CAMERA_CONTROL_PIN , 2, -1 )
 
+#define SPI3_TX_DMA_OPT      0
+#define ADC1_DMA_OPT         1
 
-#define SPI3_TX_DMA_OPT     0
-#define ADC1_DMA_OPT        1
-
-#define MAG_I2C_INSTANCE I2CDEV_1
-#define BARO_I2C_INSTANCE I2CDEV_1
-
-#define DEFAULT_BLACKBOX_DEVICE     BLACKBOX_DEVICE_FLASH
 #define DEFAULT_DSHOT_BURST DSHOT_DMAR_ON
 #define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
 #define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
 #define DEFAULT_CURRENT_METER_SCALE 490
-#define BEEPER_INVERTED
-#define FLASH_SPI_INSTANCE SPI3
-#define MAX7456_SPI_INSTANCE SPI2
-#define PINIO1_CONFIG 129
-#define PINIO1_BOX 0
-
-#define FLASH_SPI_INSTANCE SPI3
-#define GYRO_1_SPI_INSTANCE SPI1
-#define GYRO_1_ALIGN CW270_DEG_FLIP
