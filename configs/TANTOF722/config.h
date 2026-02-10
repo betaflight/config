3d4d1fbab7c691085be29f7183be4c1f525a3a43
Mark Haslinghuis <mark@numloq.nl>
2026-02-10 20:06:09 +0100

    Update configs/TANTOF722/config.h
    Co-authored-by: ot0tot <36753790+ot0tot@users.noreply.github.com>

--- configs/TANTOF722/config.h
+++ configs/TANTOF722/config.h
@@ -77,14 +77,14 @@
 #define USB_DETECT_PIN PB2
 
 #define TIMER_PIN_MAPPING \
-    TIMER_PIN_MAP( 0, PA8 , 1, 1 ) \
-    TIMER_PIN_MAP( 1, PA9 , 1, 0 ) \
-    TIMER_PIN_MAP( 2, PC8 , 2, 0 ) \
-    TIMER_PIN_MAP( 3, PC9 , 2, 0 ) \
-    TIMER_PIN_MAP( 4, PB0 , 2, 0 ) \
-    TIMER_PIN_MAP( 5, PB1 , 2, 0 ) \
-    TIMER_PIN_MAP( 6, PA3 , 2, 0 ) \
-    TIMER_PIN_MAP( 7, PB3 , 1, 0 )
+    TIMER_PIN_MAP( 0, PA8 , 1,  0 ) \
+    TIMER_PIN_MAP( 1, PA9 , 1,  0 ) \
+    TIMER_PIN_MAP( 2, PC8 , 2,  0 ) \
+    TIMER_PIN_MAP( 3, PC9 , 2,  0 ) \
+    TIMER_PIN_MAP( 4, PB0 , 2, -1 ) \
+    TIMER_PIN_MAP( 5, PB1 , 2, -1 ) \
+    TIMER_PIN_MAP( 6, PA3 , 2, -1 ) \
+    TIMER_PIN_MAP( 7, PB3 , 1,  0 )
 
 #define ADC1_DMA_OPT 1
 
