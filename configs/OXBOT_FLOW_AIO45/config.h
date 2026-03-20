00488c371209c5fb65d9b88742d45f1e123c37ed
Michael-Oxbot <dev@oxbot.com>
2026-03-20 21:18:46 +0800

    Modify TIMER_PIN_MAPPING

--- configs/OXBOT_FLOW_AIO45/config.h
+++ configs/OXBOT_FLOW_AIO45/config.h
@@ -78,10 +78,10 @@
 
 #define TIMER_PIN_MAPPING       TIMER_PIN_MAP(0, MOTOR1_PIN,        1,   1) \
                                 TIMER_PIN_MAP(1, MOTOR2_PIN,        1,   2) \
-                                TIMER_PIN_MAP(2, MOTOR3_PIN,        1,   3) \
+                                TIMER_PIN_MAP(2, MOTOR3_PIN,        2,   3) \
                                 TIMER_PIN_MAP(3, MOTOR4_PIN,        2,   4) \
                                 TIMER_PIN_MAP(4, LED_STRIP_PIN,     1,   5) \
-                                TIMER_PIN_MAP(5, GYRO_1_CLKIN_PIN,  3,  -1)
+                                TIMER_PIN_MAP(5, GYRO_1_CLKIN_PIN,  1,  -1)
 
 #define ADC1_DMA_OPT            11
 #define BEEPER_INVERTED
