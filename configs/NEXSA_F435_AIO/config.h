4bb13c89baf2416a1618fef40e04191efaed6cf8
X39A <1695404874@qq.com>
2026-06-23 16:46:41 +0800

    Add NEXSA F435AIO config

---
+++ configs/NEXSA_F435_AIO/config.h
@@ -0,0 +1,119 @@
+/*
+ * This file is part of Betaflight.
+ *
+ * Betaflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Betaflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define FC_TARGET_MCU     AT32F435G
+
+#define BOARD_NAME        NEXSA_F435_AIO
+#define MANUFACTURER_ID   NEXA
+
+#define USE_ACC
+#define USE_ACC_SPI_MPU6000
+#define USE_ACC_SPI_ICM20689
+#define USE_FLASH
+#define USE_FLASH_M25P16
+#define USE_FLASH_W25Q128FV
+#define USE_GYRO
+#define USE_GYRO_SPI_MPU6000
+#define USE_GYRO_SPI_ICM20689
+#define USE_GYRO_SPI_ICM42688P
+#define USE_ACC_SPI_ICM42688P
+#define USE_BARO
+#define USE_BARO_DPS310
+#define USE_MAX7456
+#define USE_LED_STRIP
+#define USE_MAG
+#define USE_MAG_QMC5883
+#define USE_GPS
+#define USE_SERVOS
+
+#define BEEPER_PIN           PB1
+#define MOTOR1_PIN           PC9
+#define MOTOR2_PIN           PC8
+#define MOTOR3_PIN           PC7
+#define MOTOR4_PIN           PC6
+#define SERVO1_PIN           PB4
+#define SERVO2_PIN           PB3
+
+#define LED_STRIP_PIN        PH3
+// #define UART1_TX_PIN         PA9
+#define UART2_TX_PIN         PA8
+#define UART3_TX_PIN         PC4
+#define UART4_TX_PIN         PC10
+#define UART5_TX_PIN         PC12
+#define UART1_RX_PIN         PA10
+#define UART2_RX_PIN         PB0
+#define UART3_RX_PIN         PC5
+#define UART4_RX_PIN         PC11
+#define UART5_RX_PIN         PD2
+#define INVERTER_PIN_UART1   PC0
+#define I2C2_SCL_PIN         PA0
+#define I2C2_SDA_PIN         PA1
+#define LED0_PIN             PB10
+#define LED1_PIN             PB11
+#define LED2_PIN             PB5
+#define SPI1_SCK_PIN         PA5
+#define SPI2_SCK_PIN         PB13
+#define SPI4_SCK_PIN         PB7
+#define SPI1_SDI_PIN         PA6
+#define SPI2_SDI_PIN         PB14
+#define SPI4_SDI_PIN         PB8
+#define SPI1_SDO_PIN         PA7
+#define SPI2_SDO_PIN         PB15
+#define SPI4_SDO_PIN         PB9
+// #define ESCSERIAL_PIN        PA3
+#define ADC_VBAT_PIN         PC1
+// #define ADC_RSSI_PIN         PC0
+#define ADC_CURR_PIN         PC3
+#define FLASH_CS_PIN         PB6
+#define MAX7456_SPI_CS_PIN   PA4
+#define GYRO_1_EXTI_PIN      PH2
+#define GYRO_1_CS_PIN        PB12
+
+#define PINIO1_PIN              PB5 // LT_EN
+
+#define TIMER_PIN_MAPPING \
+    TIMER_PIN_MAP( 0, PC9, 8, 4) \
+    TIMER_PIN_MAP( 1, PC8 , 8,  3) \
+    TIMER_PIN_MAP( 2, PC7 , 8,  2) \
+    TIMER_PIN_MAP( 3, PC6, 8,  1) \
+    TIMER_PIN_MAP( 4, PB4 , 3,  1) \
+    TIMER_PIN_MAP( 5, PB3 , 2,  2) \
+    TIMER_PIN_MAP( 6, PH3 , 5,  2) 
+    // TIMER_PIN_MAP( 7, PB6 , 1,  0)
+
+
+#define ADC1_DMA_OPT        1
+
+#define DEFAULT_BLACKBOX_DEVICE     BLACKBOX_DEVICE_FLASH
+#define DEFAULT_DSHOT_BURST DSHOT_DMAR_ON
+#define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
+#define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
+#define BEEPER_INVERTED
+#define SYSTEM_HSE_MHZ 8
+#define MAX7456_SPI_INSTANCE SPI1
+// #define DASHBOARD_I2C_INSTANCE I2CDEV_1
+#define BARO_I2C_INSTANCE               I2CDEV_2
+#define MAG_I2C_INSTANCE                I2CDEV_2
+#define FLASH_SPI_INSTANCE SPI4
+#define GYRO_1_SPI_INSTANCE SPI2
+#define GYRO_1_ALIGN CW270_DEG
