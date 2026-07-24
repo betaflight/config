ce6f044875c7e72483ebd15360c808e702d1d705
Matthew Selby <matthew.selby@raspberrypi.com>
2026-07-24 11:16:41 +0100

    RASPBERRY_PI_UAVFC config.mk to use TARGET_FLASH_SIZE and PICO_FLASH_DEFINES

--- configs/RASPBERRY_PI_UAVFC/config.mk
+++ configs/RASPBERRY_PI_UAVFC/config.mk
@@ -1,9 +1,12 @@
 # It's possibile to set makefile definitions here.
 # Originally used to help configure linker scripts for flash size and font allocation. See also pico_flash_mem.ld in this folder.
 
-MCU_FLASH_SIZE = 4096
+# Override MCU_FLASH_SIZE
+TARGET_FLASH_SIZE = 4096
+
+# For pico-sdk, define flash-related attributes
 # 4194304 = 4 * 1024 * 1024
-TARGET_DEVICE_FLAGS  = \
+PICO_FLASH_DEFINES = \
                    -DPICO_FLASH_SPI_CLKDIV=2 \
                    -DPICO_FLASH_SIZE_BYTES=4194304 \
                    -DPICO_BOOT_STAGE2_CHOOSE_W25Q080=1
